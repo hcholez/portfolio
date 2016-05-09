662d0f37c52db0de306624e04a9867eb50736210
hcholez@stepnet.fr <hcholez@stepnet.fr>
2016-05-09 17:21:50 +0200

    presentation et etude

--- js/script.js
+++ js/script.js
@@ -6,6 +6,39 @@ $(document).ready(function() {
       $(this).css('height', hauteurPage)
   });
 
+  $(".date li").click(function() {
+    if($(this).hasClass('ip')){
+      $('.detail-date li').css('display', 'none')
+      $('.detail-date #ip').css('display', 'block');
+      $('.etudes ul.date li').css('opacity', '0.6');
+      $('.etudes ul.date li.ip').css('opacity', '1');
+
+    } else if ($(this).hasClass('iris')) {
+      $('.detail-date li').css('display', 'none')
+      $('.detail-date #iris').css('display', 'block');
+      $('.etudes ul.date li').css('opacity', '0.6');
+      $('.etudes ul.date li.iris').css('opacity', '1');
+
+    } else if ($(this).hasClass('ub')) {
+      $('.detail-date li').css('display', 'none')
+      $('.detail-date #ub').css('display', 'block');
+      $('.etudes ul.date li').css('opacity', '0.6');
+      $('.etudes ul.date li.ub').css('opacity', '1');
+
+    } else if ($(this).hasClass('fac')) {
+      $('.detail-date li').css('display', 'none')
+      $('.detail-date #fac').css('display', 'block');
+      $('.etudes ul.date li').css('opacity', '0.6');
+      $('.etudes ul.date li.fac').css('opacity', '1');
+
+    } else if ($(this).hasClass('castel')) {
+      $('.detail-date li').css('display', 'none')
+      $('.detail-date #castel').css('display', 'block');
+      $('.etudes ul.date li').css('opacity', '0.6');
+      $('.etudes ul.date li.castel').css('opacity', '1');
+    }
+  });
+
 });
 
 
